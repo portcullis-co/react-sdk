8962268abb419282d678310bd563e9914e25fd3a
James Bohrman <james@getoasis.io>
2024-11-16 18:13:40 -0500

    tag

--- src/components/PortcullisTag.tsx
+++ src/components/PortcullisTag.tsx
@@ -12,11 +12,9 @@ export const PortcullisTag: React.FC = () => {
           "group rounded-full bg-gray-200 text-sm text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-200 dark:bg-neutral-900 dark:hover:bg-neutral-800",
         )}
       >
-        <AnimatedShinyText className="inline-flex items-center justify-center px-2 py-0.5 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
           <img src='/portcullis.svg' alt="Portcullis Logo" className="mr-2 h-6 w-6" />
-          <span>Powered by Portcullis</span>
+          <span className="text-black">Powered by Portcullis</span>
           <ArrowRightIcon className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
-        </AnimatedShinyText>
       </div>
     </div>
   );
