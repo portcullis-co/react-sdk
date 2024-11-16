ce410c181d15f788185ece02d31a044414cf5b9f
James Bohrman <james@getoasis.io>
2024-11-16 18:00:15 -0500

    tailwind

--- src/components/PortcullisTag.tsx
+++ src/components/PortcullisTag.tsx
@@ -6,13 +6,13 @@ import React from "react";
 
 export const PortcullisTag: React.FC = () => {
   return (
-    <div className="z-10 flex min-h-64 items-center justify-start">
+    <div className="z-10 flex min-h-64 items-center justify-start mx-4 my-2">
       <div
         className={cn(
-          "group rounded-full border border-black/5 bg-neutral-100 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-200 dark:border-white/5 dark:bg-neutral-900 dark:hover:bg-neutral-800",
+          "group rounded-full bg-gray-200 text-sm text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-200 dark:bg-neutral-900 dark:hover:bg-neutral-800",
         )}
       >
-        <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
+        <AnimatedShinyText className="inline-flex items-center justify-center px-2 py-0.5 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
           <img src='/portcullis.svg' alt="Portcullis Logo" className="mr-2 h-6 w-6" />
           <span>Powered by Portcullis</span>
           <ArrowRightIcon className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
